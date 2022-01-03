ecdad67f9c709e5220d64515ad86968d73de50c7
Danguole Luzaite <danguole.luzaite@gmail.com>
2022-01-03 02:07:01 +0100

    Modèle utilisateur créé, configuré les routes d'authentification

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
-
 const app = express();
 const mongoose = require('mongoose');
+
+const userRoutes = require('./routes/user');
 
 mongoose.connect('mongodb+srv://DanguoleLu:rcyX3NFCR76R9MV@cluster0.aopzj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
   { useNewUrlParser: true,
@@ -9,9 +10,12 @@ mongoose.connect('mongodb+srv://DanguoleLu:rcyX3NFCR76R9MV@cluster0.aopzj.mongod
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
- 
+
 app.use((req, res) => {
     res.json({ message: 'Votre requête a bien été reçue !' });
 });
+
+
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
