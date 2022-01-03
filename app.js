9aff6eab9967925e054d9c7375adf1a2a93b1157
Danguole Luzaite <danguole.luzaite@gmail.com>
2022-01-03 03:12:15 +0100

    Installé bcrypt; ajoutés headers pour éviter les erreurs CORS; implémentée la fonction  signup dans le contrôleur.

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const mongoose = require('mongoose');
 
 const userRoutes = require('./routes/user');
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
 mongoose.connect('mongodb+srv://DanguoleLu:rcyX3NFCR76R9MV@cluster0.aopzj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
