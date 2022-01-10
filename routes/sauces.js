12a93e9a258ee42f2f1beb452824435d182ba69b
Danguole Luzaite <danguole.luzaite@gmail.com>
2022-01-10 01:28:24 +0100

    Ajouté le middleware d'authentification pour sécuriser les routes.

--- routes/sauces.js
+++ routes/sauces.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+const auth = require('../middleware/auth');
 const saucesCtrl = require('../controllers/sauces');
 
 
-router.post('api/sauces', saucesCtrl.createSauce);
-router.get('/api/sauces/', saucesCtrl.getAllSauces);
-router.get('/api/sauces/:id', saucesCtrl.getOneSauce);
-router.put('/api/sauces/:id', saucesCtrl.modifySauce);
-router.delete('/api/sauces/:id', saucesCtrl.deleteSauce);
+router.post('api/sauces', auth, saucesCtrl.createSauce);
+router.get('/api/sauces/', auth, saucesCtrl.getAllSauces);
+router.get('/api/sauces/:id', auth, saucesCtrl.getOneSauce);
+router.put('/api/sauces/:id', auth, saucesCtrl.modifySauce);
+router.delete('/api/sauces/:id', auth, saucesCtrl.deleteSauce);
 
 
 module.exports = router;
